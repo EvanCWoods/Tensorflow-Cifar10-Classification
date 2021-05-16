625fa6b97deb154e9f2adfd1122375a1cba12dff
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2021-05-17 09:25:16 +1000

    Update Data-Exploration.py
    This script gives insight into the data and is a testing ground for the kind of preprocessing that will be necessary before feeding it to a model.

--- Data-Exploration.py
+++ Data-Exploration.py
@@ -1,1 +1,53 @@
+# Import libraries
+import tensorflow as tf
+import keras
+import random
+import matplotlib.pyplot as plt
+
+# Create the datasets
+(train_data, train_labels), (test_data, test_labels) = tf.keras.datasets.cifar10.load_data()
+
+# Function to explore the raw data's attributes
+def data_info(train_data, test_data):
+    print('Train data dtype: ', train_data.dtype)
+    print('Test data dtype: ', test_data.dtype)
+    print('Train data min: ', train_data.min())
+    print('Train data max: ', train_data.max())
+    print('Test data min: ', test_data.min())
+    print('Test data max: ', test_data.max())
+    print('Train data shape: ', train_data.shape)
+    print('Test data shape: ', test_data.shape)
+
+# Show the raw data's attributes
+data_info(train_data=train_data, test_data=test_data)
+
+
+# Function to preprocess the data
+def preprocess(train_data, test_data):
+    train_data = train_data / 255
+    print('Train data min: ', train_data.min())
+    print('Train data max: ', train_data.max())
+    test_data = test_data / 255
+    print('Test data min: ', test_data.min())
+    print('Test data max: ', test_data.max())
+    train_data = train_data.reshape(-1,32,32,3)
+    print('Train data shape: ', train_data.shape)
+    test_data = test_data.reshape(-1,32,32,3)
+    print('Test data shape: ', test_data.shape)
+    train_data = train_data.astype('float32')
+    print(train_data.dtype)
+    test_data = test_data.astype('float32')
+    print(test_data.dtype)
+
+# Preprocess the data
+preprocess(train_data=train_data, test_data=test_data)
+
+# Show the images
+def show_images(train_data):
+    i = random.randint(0, train_data.shape[0])
+
+    plt.imshow(train_data[i])
+    plt.title(train_labels[i])
+
+show_images(train_data=train_data)
 
